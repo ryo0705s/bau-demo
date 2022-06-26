c7c99888b48d44281b93f9609aea2cd8e8a860e5
ryo0705s <inspire.sasaki15@gmail.com>
2022-06-26 16:19:31 +0900

    不完全だが一旦propsとemitのトレーニングは終了する

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-    <Pro count="0" />
+    <!-- <Pro count="0" /> -->
+    <Pro :count="state.kazoeru" />
+    <!-- <div>{{state.kazoeru}}</div> -->
+    <button @click="add">増やす</button>
+    <!-- <div>{{ state.text }}</div> -->
+    <input v-model="text" />
+    <button :text="textee" @deletee="erase">消す</button>
   </div>
 </template>
 <script>
+import { ref, reactive } from "@nuxtjs/composition-api";
 import Pro from "./pro.vue";
 export default {
   components: {
     Pro,
+  },
+  setup() {
+    const state = reactive({
+      kazoeru: 0,
+    });
+    const add = () => {
+      state.kazoeru++;
+      console.log(state.kazoeru);
+    };
+    const erase = () => {
+      textee.value = "";
+    };
+    return {
+      state,
+      add,
+      erase,
+    };
   },
 };
 </script>
--- pages/pro.vue
+++ pages/pro.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <div>{{ count }}</div>
+    <div>{{ text }}</div>
+    <button @click="deletee"></button>
   </div>
 </template>
 <script>
+import { ref, reactive } from "@nuxtjs/composition-api";
 export default {
   props: ["count"],
-  setup(props) {},
+  setup(props, context) {
+    const state = reactive({});
+    const deletee = context.emit("deletee");
+    return {
+      deletee,
+    };
+  },
 };
 </script>
